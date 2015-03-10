38a2df79d388ed89fa834e0c8456a87901ac7856
Alan Litteneker <alan.litteneker@gmail.com>
2015-03-09 18:26:03 -0700

    Addition of newest analysis scripts

--- core/instrument/analyze.pl
+++ core/instrument/analyze.pl
@@ -1,32 +1,23 @@
 use strict;
 use warnings;
-
-# Currently outputs:
-#   (1) stack.csv
-#       (a) stack id
-#       (b) all remaining cells will contain method calls
-#   (2) stats.csv
-#       (a) stack id
-#       (b) passes
-#       (c) fails
-#       (d) Tarantula core
-#       (e) SBI score
-#       (f) Jaccard score
-#       (g) Ochiai score
-#       (h) Cumulative cyclomatic complexity (computed from $complexities file below)
+use List::MoreUtils qw(any first_result);
 
 my $prefix = './cct/';
 my $output = './results/';
-my $complexities = "./out.csv";
+my $metadata = "./ep_static_outer.csv";
 my $pattern = quotemeta "com/google/errorprone/";
-my @stat_order = qw( pass fail tarantula sbi jaccard ochiai );
-my $order_key = "tarantula";
+my @order_keys = qw( m_tarantula tarantula total );
+my @stat_order = (
+    qw( pass fail m_pass m_fail line_var pass_line_var fail_line_var ubiquity method_ubiquity line_method_ubiquity ),
+    qw( fail_iso_1 fail_iso_2 m_tarantula tarantula sbi jaccard ochiai ) # and more from metadata file
+);
 
 # -- Begin Script --
 my %collected_method_stacks = ();
 my %collected_line_stacks = ();
 my %collected_methods = ();
 my %collected_lines = ();
+my %line_stats = ();
 my %test_totals = ();
 
 for my $type ( qw( pass fail ) ) {
@@ -53,6 +44,8 @@ for my $type ( qw( pass fail ) ) {
     }
 }
 
+build_line_stats();
+
 my %metadata = load_metadata();
 print "All input files processed.\n";
 
@@ -64,14 +57,51 @@ write_data( 1, "line", %collected_lines );
 print "Complete! All output written to directory $output.\n";
 # -- End Script --
 
+# build an additional data structure for method line variability
+sub build_line_stats {
+    for my $key ( keys %collected_lines ) {
+        my ( $meth, $ln ) = split ',', $key;
+        
+        $line_stats{$meth} //= { lines => {}, total_pass => 0, total_fail => 0 };
+        $line_stats{$meth}->{lines}->{$ln} = {
+            pass => $collected_lines{$key}->{pass},
+            fail => $collected_lines{$key}->{fail}
+        };
+        $line_stats{$meth}->{total_pass} += $collected_lines{$key}->{pass};
+        $line_stats{$meth}->{total_fail} += $collected_lines{$key}->{fail};
+    }
+    for my $meth ( keys %line_stats ) {
+        my $num_lines = scalar keys %{ $line_stats{$meth}->{lines} };
+        my $total = $line_stats{$meth}->{total_pass} + $line_stats{$meth}->{total_fail};
+        my $avg = $total / $num_lines;
+        my $avg_pass = $line_stats{$meth}->{total_pass} / $num_lines;
+        my $avg_fail = $line_stats{$meth}->{total_fail} / $num_lines;
+        my ( $diff, $pass_diff, $fail_diff ) = ( 0, 0, 0 );
+        
+        for my $lnkey ( keys %{ $line_stats{$meth}->{lines} } ) {
+            $diff    += ( ( $line_stats{$meth}->{lines}->{$lnkey}->{pass}
+                          + $line_stats{$meth}->{lines}->{$lnkey}->{fail} ) - $avg ) ** 2;
+            $pass_diff += ( $line_stats{$meth}->{lines}->{$lnkey}->{pass} - $avg_pass ) ** 2;
+            $fail_diff += ( $line_stats{$meth}->{lines}->{$lnkey}->{fail} - $avg_fail ) ** 2;
+        }
+        
+        $line_stats{$meth}->{num_lines} = $num_lines;
+        $line_stats{$meth}->{avg} = $avg;
+        $line_stats{$meth}->{avg_pass} = $avg_pass;
+        $line_stats{$meth}->{avg_fail} = $avg_fail;
+        $line_stats{$meth}->{stdev} = sqrt( $diff / $num_lines );
+        $line_stats{$meth}->{stdev_pass} = sqrt( $pass_diff / $num_lines );
+        $line_stats{$meth}->{stdev_fail} = sqrt( $fail_diff / $num_lines );
+    }
+}
+
+# increment: helper function to let us more easily build our data structures
 sub increment {
     my ( $type, $source, $destination ) = @_;
     
     for my $key ( keys %{ $source } ) {
-        if( not(exists $destination->{ $key } )) {
-            $destination->{ $key } = { pass => 0, fail => 0 };
-        }
-        $destination->{ $key }->{ $type } += 1;
+        $destination->{ $key } //= { pass => 0, fail => 0 };
+        $destination->{ $key }->{ $type } += $source->{ $key };
     }
 }
 
@@ -96,6 +126,7 @@ sub process_file {
         
         my ( $call_type, $id ) = ( $line =~ m/(\S+)\s(\S+)/ );
         if( $call_type eq 'CALL' ) {
+            $id =~ s/<init>/Constructor/;
             push @methods, $id;
             $lineset[ ++$index ] = -1;
             push @active, 0;
@@ -131,38 +162,47 @@ sub write_data {
     
     my $index = 0;
     my @keys = keys %data;
+    my $num_keys = scalar(@keys);
     
-    print "$type:\tProcessing output of " . scalar(@keys) . " results.\n";
+    print "$type:\tProcessing output of $num_keys results.\n";
     
     for my $key ( @keys ) {
-        compute_scores( $data{$key}, %test_totals );
-        $data{$key}->{'cyclomatic_complexity'} = get_metadata( $key, %metadata );
+        compute_scores( $type, $key, $data{$key}, %test_totals );
+        get_metadata( $key, $data{$key}, %metadata );
         if( ++$index % 1000 == 0 ) {
-            print "\t\t\tFinished $index out of " . scalar(@keys) . " total results.\n";
+            print "\t\t\tFinished $index out of $num_keys total results.\n";
         }
     }
+    
+    my @this_stat_order = grep { my $k = $_; any { defined( $data{$_}->{$k} ) } @keys } @stat_order;
+    
+    my @orderers = grep { defined($data{@keys[0]}->{$_}) } @order_keys;
+    print "$type:\tSorting $num_keys by @orderers.\n";
     @keys = sort {
-        $data{$b}->{$order_key} <=> $data{$a}->{$order_key}
-        or $data{$b}->{pass} + $data{$b}->{fail} <=> $data{$a}->{pass} + $data{$a}->{fail}
+        ( first_result { $data{$b}->{$_} <=> $data{$a}->{$_} } @orderers ) // 0
     } @keys;
 
-    print "$type:\tStarting output of " . scalar(@keys) . " results.\n";
+    print "$type:\tStarting output of $num_keys results.\n";
 
     my ( $stacks, $stats );
     unless( $use_key ) {
         open($stacks, ">${output}${type}_stacks.csv") or die "Could not open ${type}_stacks.csv $!";
     }
     open($stats,  ">${output}${type}_stats.csv") or die "Could not open ${type}_stats.csv $!";
+    
+    # print header row for stats file
+    print $stats join( ',', map { ucfirst( lc( $_ ) ) }
+        ( ( $use_key ? ($type eq 'line' ? ('method', 'line') : 'method') : 'stack_id' ), @this_stat_order ) ) . "\n";
 
     $index = 0;
     while( $index < scalar(@keys) ) {
         
         unless( $use_key ) {
-            print $stacks ( $index + 1 ) . "," . $keys[$index] . "\n";
+            print $stacks ( $index + 1 ) . ",$keys[$index]\n";
         }
         print $stats join( ',',
             ( $use_key ? $keys[$index] : ( $index + 1 ),
-            map { $data{$keys[$index]}->{$_} } @stat_order )
+            map { $data{$keys[$index]}->{$_} // 'NA' } @this_stat_order )
         ) . "\n";
         
         ++$index;
@@ -188,45 +228,60 @@ sub get_test_filenames {
 
 # load_metadata: load cyclomatic complexity data
 sub load_metadata {
-    print "Loading cyclomatic complexity metadata . . . ";
+    print "Loading metadata . . . ";
     
     my %ret = ();
     
-    open(my $fh, '<:encoding(UTF-8)', $complexities) or die "Could not open file '$complexities' $!";
+    open(my $fh, '<:encoding(UTF-8)', $metadata) or die "Could not open file '$metadata' $!";
+    
+    chomp( my $header = <$fh> );
+    my @keys = map { $_ =~ s/['"]//rg } (split ',', $header)[1, -1];
+    
     while (my $line = <$fh>) {
         chomp $line;
         
-        my ( $id, $data ) = ( $line =~ m/(.+?),(\d+)/ );
-        $ret{$id} = $data;
+        my ( $id, $data ) = ( $line =~ m/(.+?),([\d.,]+)/ );
+        $id =~ s/['"]//g;
+        my $index = 0;
+        
+        $ret{$id} = {};
+        for my $val ( split ',', $data ) {
+            $ret{$id}->{ $keys[$index++] } = $val;
+        }
     }
+    
+    push @stat_order, @keys;
     
     print "loaded.\n";
     
     return %ret;
 }
 
+# aggregate metadata for a particular key/stack
 sub get_metadata {
-    my ( $stack, %data ) = @_;
+    my ( $key, $data, %metrics ) = @_;
     
-    my @stack_vals = split ',', $stack;
-    my $result = 0;
+    my @stack_vals = split ',', $key;
     
     for my $id ( @stack_vals ) {
-        ( $id ) = ( $id =~ m/\/([^\/]+?)(?::\d+)?$/ );
-        if( length($id) and exists($data{$id}) ) {
-            $result += $data{$id};
+        ( $id ) = ( $id =~ m/\/([^\/]+?)(?:[,:]\d+)?$/ );
+        if( length($id) and exists($metrics{$id}) ) {
+            for my $key ( keys %{ $metrics{$id} } ) {
+                $data->{$key} = ($data->{$key} // 0) + $metrics{$id}->{$key};
+            }
         }
     }
-    
-    return $result;
 }
 
 sub compute_scores {
-    my ( $data, %test_totals ) = @_;
+    my ( $type, $key, $data, %test_totals ) = @_;
 
     my ( $pass, $fail ) = ( $data->{pass}, $data->{fail} );
     my $perc_pass = ( $pass / $test_totals{pass} );
     my $perc_fail = ( $fail / $test_totals{fail} );
+    
+    # write total to make sorting a bit easier
+    $data->{'total'} = $pass + $fail;
     
     # Tarantula: (%failed) / (%passed + %failed)
     $data->{'tarantula'} = ( $perc_fail / ( $perc_pass  + $perc_fail ) );
@@ -239,6 +294,56 @@ sub compute_scores {
     
     # Ochiai: failed / sqrt( total_failed * ( passed + failed ) )
     $data->{'ochiai'} = ( $fail / sqrt( $test_totals{fail} * ( $pass + $fail ) ) );
+    
+    # Ubiquity: ( pass + fail ) / ( total_pass + total_fail )
+    $data->{'ubiquity'} = ( $pass + $fail ) / ( $test_totals{pass} + $test_totals{fail} );
+    
+    # Method Stats
+    my ( $m_string, $m_pass, $m_fail );
+    if( $type =~ /stack/ ) {
+        $m_string = join ',', map /^([^:]+)/, split ',', $key;
+        $m_pass = $collected_method_stacks{$m_string}->{pass};
+        $m_fail = $collected_method_stacks{$m_string}->{fail};
+    }
+    else {
+        ( $m_string ) = ( $key =~ m/^([^:,]+?)(?:,\d+)?$/ );
+        $m_pass = $collected_methods{$m_string}->{pass};
+        $m_fail = $collected_methods{$m_string}->{fail};
+    }
+
+    exit 1 unless length($m_string) and ( $m_pass + $m_fail );
+    $data->{"m_pass"} = $m_pass;
+    $data->{"m_fail"} = $m_fail;
+    my ( $m_perc_pass, $m_perc_fail ) = ( $pass / ($m_pass or 1), $fail / ($m_fail or 1) );
+    
+    # Method Ubiquity: compute percentage of tests in which this method is called
+    $data->{'method_ubiquity'} = ( $m_pass + $m_fail ) / ( $test_totals{pass} + $test_totals{fail} );
+    
+    # Line Method Ubiquity: compute percentage of time this is called when the enclosing method is called
+    $data->{'line_method_ubiquity'} = ( $pass + $fail ) / ( $m_pass + $m_fail );
+    
+    # Method Tarantula: same as normal tarantula, but with method totals rather than test totals
+    $data->{'m_tarantula'} = $m_perc_fail / ( $m_perc_pass + $m_perc_fail );
+    
+    if( $type eq 'line' ) {
+        # Line Variability: difference from average number of line executions for this method, normalized by stdev
+        $data->{'line_var'} =
+            ( ( $pass + $fail ) - $line_stats{$m_string}->{avg} ) / ( $line_stats{$m_string}->{stdev} or 1 );
+        
+        # Pass Line Variability: difference from average number of line passes for this method, normalized by stdev
+        $data->{'pass_line_var'} =
+            ( $pass - $line_stats{$m_string}->{avg_pass} ) / ( $line_stats{$m_string}->{stdev_pass} or 1 );
+        
+        # Fail Line Variability: difference from average number of line fails for this method, normalized by stdev
+        $data->{'fail_line_var'} =
+            ( $fail - $line_stats{$m_string}->{avg_fail} ) / ( $line_stats{$m_string}->{stdev_fail} or 1 );
+        
+        # Fail Isolation: attempt to rank failures by variability of specific line execution
+        $data->{'fail_iso_1'} = $data->{'tarantula'} * $data->{'fail_line_var'};
+        $data->{'fail_iso_2'} = $data->{'m_tarantula'} * $data->{'fail_line_var'};
+    }
+    
+    # TODO: Something more interesting, use ubiquity as further dimension for metric
 }
 
 
